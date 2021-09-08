9975e2d02d6447f97dd0ee78a8654a3a0d3b668f
Angel Martínez <angel.martinez@wizeline.com>
2021-09-07 21:22:42 -0500

    Add graphics

---
+++ app-6-data-visualization/index.py
@@ -0,0 +1,531 @@
+import justpy as jp
+import pandas
+
+data = pandas.read_csv("reviews.csv", parse_dates=['Timestamp'])
+data['Day'] = data['Timestamp'].dt.date
+day_average = data.groupby(['Day']).mean()
+data['Month'] = data['Timestamp'].dt.month
+month_average = data.groupby(['Month', 'Course Name'])['Rating'].mean().unstack()
+share = data.groupby(['Course Name'])['Rating'].count()
+chart_def = """
+{
+  chart: {
+    type: 'spline',
+    inverted: false
+  },
+  title: {
+    text: 'Atmosphere Temperature by Altitude'
+  },
+  subtitle: {
+    text: 'According to the Standard Atmosphere Model'
+  },
+  xAxis: {
+    reversed: false,
+    title: {
+      enabled: true,
+      text: 'Date'
+    },
+    labels: {
+      format: '{value}'
+    },
+    accessibility: {
+      rangeDescription: 'Range: 0 to 80 km.'
+    },
+    maxPadding: 0.05,
+    showLastLabel: true
+  },
+  yAxis: {
+    title: {
+      text: 'Average Rating'
+    },
+    labels: {
+      format: '{value}'
+    },
+    accessibility: {
+      rangeDescription: 'Range: -90°C to 20°C.'
+    },
+    lineWidth: 2
+  },
+  legend: {
+    enabled: false
+  },
+  tooltip: {
+    headerFormat: '<b>{series.name}</b><br/>',
+    pointFormat: '{point.x} {point.y}'
+  },
+  plotOptions: {
+    spline: {
+      marker: {
+        enable: false
+      }
+    }
+  },
+  series: [{
+    name: 'Average Rating',
+    data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+      [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+  }]
+}
+"""
+
+chart_def2 = """
+{
+    chart: {
+        type: 'spline'
+    },
+    title: {
+        text: 'Average fruit consumption during one week'
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 150,
+        y: 100,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: '#FFFFFF'
+    },
+    xAxis: {
+        categories: [
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday'
+        ],
+        plotBands: [{ // visualize the weekend
+            from: 4.5,
+            to: 6.5,
+            color: 'rgba(68, 170, 213, .2)'
+        }]
+    },
+    yAxis: {
+        title: {
+            text: 'Fruit units'
+        }
+    },
+    tooltip: {
+        shared: true,
+        valueSuffix: ' units'
+    },
+    credits: {
+        enabled: false
+    },
+    plotOptions: {
+        areaspline: {
+            fillOpacity: 0.5
+        }
+    },
+    series: [{
+        name: 'John',
+        data: [3, 4, 3, 5, 4, 10, 12]
+    }, {
+        name: 'Jane',
+        data: [1, 3, 4, 3, 3, 5, 4]
+    }]
+}
+"""
+
+chart_def3 = """
+{
+
+    chart: {
+        type: 'streamgraph',
+        marginBottom: 30,
+        zoomType: 'x'
+    },
+
+    title: {
+        floating: true,
+        align: 'left',
+        text: 'Winter Olympic Medal Wins'
+    },
+    subtitle: {
+        floating: true,
+        align: 'left',
+        y: 30,
+        text: 'Source: <a href="https://www.sports-reference.com/olympics/winter/1924/">sports-reference.com</a>'
+    },
+
+    xAxis: {
+        maxPadding: 0,
+        type: 'category',
+        crosshair: true,
+        categories: [],
+        labels: {
+            align: 'left',
+            reserveSpace: false,
+            rotation: 270
+        },
+        lineWidth: 0,
+        margin: 20,
+        tickWidth: 0
+    },
+
+    yAxis: {
+        visible: false,
+        startOnTick: false,
+        endOnTick: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    annotations: [{
+        labels: [{
+            point: {
+                x: 5.5,
+                xAxis: 0,
+                y: 30,
+                yAxis: 0
+            },
+            text: 'Cancelled<br>during<br>World War II'
+        }, {
+            point: {
+                x: 18,
+                xAxis: 0,
+                y: 90,
+                yAxis: 0
+            },
+            text: 'Soviet Union fell,<br>Germany united'
+        }],
+        labelOptions: {
+            backgroundColor: 'rgba(255,255,255,0.5)',
+            borderColor: 'silver'
+        }
+    }],
+
+    plotOptions: {
+        series: {
+            label: {
+                minFontSize: 5,
+                maxFontSize: 15,
+                style: {
+                    color: 'rgba(255,255,255,0.75)'
+                }
+            }
+        }
+    },
+
+    // Data parsed with olympic-medals.node.js
+    series: [{
+        name: "Finland",
+        data: [
+            0, 11, 4, 3, 6, 0, 0, 6, 9, 7, 8, 10, 5, 5, 7, 9, 13, 7, 7, 6, 12, 7, 9, 5, 5
+        ]
+    }, {
+        name: "Austria",
+        data: [
+            0, 3, 4, 2, 4, 0, 0, 8, 8, 11, 6, 12, 11, 5, 6, 7, 1, 10, 21, 9, 17, 17, 23, 16, 17
+        ]
+    }, {
+        name: "Sweden",
+        data: [
+            0, 2, 5, 3, 7, 0, 0, 10, 4, 10, 7, 7, 8, 4, 2, 4, 8, 6, 4, 3, 3, 7, 14, 11, 15
+        ]
+    }, {
+        name: "Norway",
+        data: [
+            0, 17, 15, 10, 15, 0, 0, 10, 16, 4, 6, 15, 14, 12, 7, 10, 9, 5, 20, 26, 25, 25, 19, 23, 26
+        ]
+    }, {
+        name: "U.S.",
+        data: [
+            0, 4, 6, 12, 4, 0, 0, 9, 11, 7, 10, 7, 7, 8, 10, 12, 8, 6, 11, 13, 13, 34, 25, 37, 28
+        ]
+    }, {
+        name: "East Germany",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 14, 19, 23, 24, 25, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "West Germany",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 5, 10, 5, 4, 8, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Germany",
+        data: [
+            0, 0, 1, 2, 6, 0, 0, 0, 7, 2, 8, 9, 0, 0, 0, 0, 0, 0, 26, 24, 29, 36, 29, 30, 19
+        ]
+    }, {
+        name: "Netherlands",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 2, 2, 9, 9, 6, 4, 0, 7, 4, 4, 11, 8, 9, 8, 24
+        ]
+    }, {
+        name: "Italy",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 1, 4, 4, 5, 4, 2, 2, 5, 14, 20, 10, 13, 11, 5, 8
+        ]
+    }, {
+        name: "Canada",
+        data: [
+            0, 1, 1, 7, 1, 0, 0, 3, 2, 3, 4, 3, 3, 1, 3, 2, 4, 5, 7, 13, 15, 17, 24, 26, 25
+        ]
+    }, {
+        name: "Switzerland",
+        data: [
+            0, 3, 1, 1, 3, 0, 0, 10, 2, 6, 2, 0, 6, 10, 5, 5, 5, 15, 3, 9, 7, 11, 14, 9, 11
+        ]
+    }, {
+        name: "Great Britain",
+        data: [
+            0, 4, 1, 0, 3, 0, 0, 2, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 2, 1, 2, 1, 1, 4
+        ]
+    }, {
+        name: "France",
+        data: [
+            0, 3, 1, 1, 1, 0, 0, 5, 1, 0, 3, 7, 9, 3, 1, 1, 3, 2, 9, 5, 8, 11, 9, 11, 15
+        ]
+    }, {
+        name: "Hungary",
+        data: [
+            0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Unified Team",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Soviet Union",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 21, 25, 13, 16, 27, 22, 25, 29, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Russia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23, 18, 13, 22, 15, 33
+        ]
+    }, {
+        name: "Japan",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 1, 1, 1, 7, 5, 10, 2, 1, 5, 8
+        ]
+    }, {
+        name: "Czechoslovakia",
+        data: [
+            0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 4, 3, 1, 1, 6, 3, 3, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Poland",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 2, 6, 6
+        ]
+    }, {
+        name: "Spain",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "China",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 8, 8, 11, 11, 9
+        ]
+    }, {
+        name: "South Korea",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 6, 4, 11, 14, 8
+        ]
+    }, {
+        name: "Czech Republic",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 4, 6, 8
+        ]
+    }, {
+        name: "Belarus",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 3, 6
+        ]
+    }, {
+        name: "Kazakhstan",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 1, 1
+        ]
+    }, {
+        name: "Bulgaria",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 3, 1, 0, 0
+        ]
+    }, {
+        name: "Denmark",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Ukraine",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 2, 0, 2
+        ]
+    }, {
+        name: "Australia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3
+        ]
+    }, {
+        name: "Belgium",
+        data: [
+            0, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Romania",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Liechtenstein",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Yugoslavia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Luxembourg",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "New Zealand",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "North Korea",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+        ]
+    }, {
+        name: "Slovakia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 1
+        ]
+    }, {
+        name: "Croatia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 3, 1
+        ]
+    }, {
+        name: "Slovenia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 3, 8
+        ]
+    }, {
+        name: "Latvia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 4
+        ]
+    }, {
+        name: "Estonia",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 1, 0
+        ]
+    }, {
+        name: "Uzbekistan",
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0
+        ]
+    }],
+
+    exporting: {
+        sourceWidth: 800,
+        sourceHeight: 600
+    }
+
+}
+"""
+
+chart_def4 = """
+{
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares in January, 2018'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Sogou Explorer',
+            y: 1.64
+        }, {
+            name: 'Opera',
+            y: 1.6
+        }, {
+            name: 'QQ',
+            y: 1.2
+        }, {
+            name: 'Other',
+            y: 2.61
+        }]
+    }]
+}
+"""
+
+def app():
+  wp = jp.QuasarPage()
+  jp.QDiv(a=wp, text='Analysis of Course Reviews', classes='text-h1 text-right q-pa-md')
+  jp.QDiv(a=wp, text='These graphs represent course review anaylsis')
+  hc = jp.HighCharts(a=wp, options=chart_def)
+  hc.options.xAxis.categories = list(day_average.index)
+  hc.options.series[0].data = list(day_average['Rating'])
+  hc2 = jp.HighCharts(a=wp, options=chart_def2)
+  hc2.options.xAxis.categories = list(month_average.index)
+  hc2_data = [{ "name": v1, "data": [v2  for v2 in  month_average[v1]] } for v1 in month_average.columns ]
+  hc2.options.series = hc2_data
+  hc3 = jp.HighCharts(a=wp, options=chart_def3)
+  hc3.options.xAxis.categories = list(month_average.index)
+  hc3_data = [{ "name": v1, "data": [v2  for v2 in  month_average[v1]] } for v1 in month_average.columns ]
+  hc3.options.series = hc3_data
+  hc4 = jp.HighCharts(a=wp, options=chart_def4)
+  hc4_data = [{ "name": v1, "y": v2 }  for v1,v2 in zip(share.index, share) ]
+  hc4.options.series[0].data = hc4_data
+  return wp
+
+jp.justpy(app)
